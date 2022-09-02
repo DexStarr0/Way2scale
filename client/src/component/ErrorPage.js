ddb9936fd1213c4244a7a721e6b339bbec45bf69
Ayush Raj <ayushraj7262@gmail.com>
2022-09-02 16:20:01 +0530

    fixed error page

--- client/src/component/ErrorPage.js
+++ client/src/component/ErrorPage.js
@@ -1,12 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
+import errImg from "../img/404Error.gif";
 export default function ErrorPage() {
   const navigate = useNavigate();
 
   return (
     <>
       <motion.div
-        className="d-flex align-items-center justify-content-center vh-100"
+        className="profile_container "
         initial={{ y: 20, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         exit={{ y: -20, opacity: 0 }}
@@ -15,43 +16,51 @@ export default function ErrorPage() {
           ease: "easeInOut",
         }}
       >
-        <div className="text-center row">
-          <div className=" col-md-6">
+        <div className="error_box text-center">
+          <div className=" ">
             <img
-              src="https://cdn.pixabay.com/photo/2017/03/09/12/31/error-2129569__340.jpg"
+              src={errImg}
+              style={{ width: "400px" }}
               alt=""
               className="img-fluid"
             />
-          </div>
-          <div className=" col-md-6 mt-5">
-            <p className="fs-3">
-              {" "}
-              <span className="text-danger">Opps!</span> Page not found.
-            </p>
-            <p className="lead">The page you’re looking for doesn’t exist.</p>
-            <div className="d-flex justify-content-center flex-wrap">
-              <button
-                className="form-btn btn  m-2"
-                onClick={() => {
-                  navigate("/");
-                }}
-              >
-                <span>
-                  Home
-                  <i className="fa fa-long-arrow-right" aria-hidden="true"></i>
-                </span>
-              </button>
-              <button
-                className="form-btn btn btn-secondary m-2"
-                onClick={() => {
-                  navigate(-1);
-                }}
-              >
-                <span>
-                  Back
-                  <i className="fa fa-long-arrow-right" aria-hidden="true"></i>
-                </span>
-              </button>
+            <div className="mt-3 d-flex align-items-center justify-content-center">
+              <div>
+                <p className="fs-3">
+                  <span className="text-danger">Opps!</span> Page not found.
+                </p>
+
+                <div className="d-flex justify-content-center flex-wrap">
+                  <button
+                    className="form-btn btn  m-2"
+                    onClick={() => {
+                      navigate("/");
+                    }}
+                  >
+                    <span>
+                      Home
+                      <i
+                        className="fa fa-long-arrow-right"
+                        aria-hidden="true"
+                      ></i>
+                    </span>
+                  </button>
+                  <button
+                    className="form-btn btn btn-secondary m-2"
+                    onClick={() => {
+                      navigate(-1);
+                    }}
+                  >
+                    <span>
+                      Back
+                      <i
+                        className="fa fa-long-arrow-right"
+                        aria-hidden="true"
+                      ></i>
+                    </span>
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>

d4f51a3126cb7ce654780df73993b546b7f8b8be
Ayush Raj <ayushraj7262@gmail.com>
2022-09-04 15:59:52 +0530

    midified explore circle size

--- client/src/component/Explore.js
+++ client/src/component/Explore.js
@@ -23,7 +23,7 @@ export default function Explore() {
           className="box container-fluid bg-warning "
           style={setdi}
           animate={{
-            scale: [1, 2, 2, 1, 1],
+            scale: [1, 1.5, 1.5, 1, 1],
             rotate: [0, 0, 180, 180, 0],
             borderRadius: ["0%", "0%", "50%", "50%", "0%"],
           }}

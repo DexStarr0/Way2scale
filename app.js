c475efaff99a48c67ad0e31f997236bbb06d6bed
Ayush Raj <ayushraj7262@gmail.com>
2023-01-17 00:39:37 +0530

    requied changes for deployment on new shosty

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+const path = require("path");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -15,14 +16,13 @@ dotenv.config({ path: "./config.env" });
 const PORT = process.env.PORT || 5000;
 require("./db/conn");
 
-//server static assets if in production
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
-  });
-}
+//static files
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 //we link the router files to make our route easy
 app.use(require("./router/auth"));
 app.use(require("./router/findjobs"));
